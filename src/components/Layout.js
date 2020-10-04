afee1d640e8d0f159344941b6b9211b7b22f0e65
Thomas Song <ysthomassong@gmail.com>
2020-10-04 16:41:09 -0700

    component update with styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
+import styled from 'styled-components';
+
+const SiteBorderStyles = styled.div`
+  max-width: 1000px;
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw, 12rem);
+  background: red;
+  padding: 5px;
+  padding: clamp(5px, 1vw, 25px);
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.044);
+`;
+
+const ContentStyles = styled.div`
+  background: white;
+  padding: 2rem;
+`;
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <Nav />
-      {children}
-      <Footer />
-    </div>
+    <>
+      <GlobalStyles />
+      <Typography />
+      <SiteBorderStyles>
+        <ContentStyles>
+          <Nav />
+          {children}
+          <Footer />
+        </ContentStyles>
+      </SiteBorderStyles>
+    </>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,48 @@
 import React from 'react';
 import { Link, navigate } from 'gatsby';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+  .logo {
+    transform: translateY(-25%);
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+    text-align: center;
+    list-style: none;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+    a {
+      font-size: 3rem;
+      text-decoration: none;
+      &:hover {
+        color: var(--red);
+      }
+    }
+  }
+`;
 
 export default function Nav() {
   const handleSliceMasters = (e) => {
@@ -10,7 +53,7 @@ export default function Nav() {
   };
 
   return (
-    <nav>
+    <NavStyles>
       <ul>
         <li>
           <Link to="/">Hot Now</Link>
@@ -19,7 +62,9 @@ export default function Nav() {
           <Link to="/pizzas">Pizza Menu</Link>
         </li>
         <li>
-          <Link to="/">Logo</Link>
+          <Link to="/">
+            <Logo />
+          </Link>
         </li>
         <li>
           <Link to="/slicemasters">Slice Masters</Link>
@@ -31,6 +76,6 @@ export default function Nav() {
           <Link to="/order">Orders</Link>
         </li>
       </ul>
-    </nav>
+    </NavStyles>
   );
 }
